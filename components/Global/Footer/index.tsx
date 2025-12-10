e6ab5897005723a4e1ac340577199ce8052177ca
Yasmin <yasminsdcastro@gmail.com>
2025-12-09 22:19:20 -0300

    feat: versão

--- components/Global/Footer/index.tsx
+++ components/Global/Footer/index.tsx
@@ -3,15 +3,18 @@
 export default function Footer() {
   return (
     <footer className="w-full border-t border-gray-400 py-4">
-      <div className="text-center text-sm text-muted-foreground">
-        <a
-          href="https://yascastro.com.br/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-foreground transition-colors cursor-pointer"
-        >
-          © {new Date().getFullYear()} Yas Castro™.
-        </a>
+      <div className="text-center text-sm text-muted-foreground space-y-1">
+        <div>
+          <a
+            href="https://yascastro.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-foreground transition-colors cursor-pointer"
+          >
+            © {new Date().getFullYear()} Yas Castro™.
+          </a>
+        </div>
+        <div>2.0</div>
       </div>
     </footer>
   );
